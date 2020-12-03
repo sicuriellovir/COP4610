fbdc45c0c303ec266fb9c9903ff4c132151d9791
sicuriellovir <54972072+sicuriellovir@users.noreply.github.com>
2020-12-02 22:24:43 -0500

    Update cmdclose.h

--- Project3/cmdclose.h
+++ Project3/cmdclose.h
@@ -3,5 +3,5 @@
 
 #include "helpers.h"
 
-void cmdclose(unsigned int pwdStartCluster, char* dirName, int fatFile_fp, struct BPBInfo* info, struct FileFAT *head, struct FileFAT *ptr);
+void cmdclose(unsigned int pwdStartCluster, char* dirName, int fatFile_fp, struct BPBInfo* info, struct openFile *head, struct openFile *ptr);
 #endif
