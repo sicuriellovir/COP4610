bcd2a2db4d45939d35072f4b710805bde0fd515c
sicuriellovir <54972072+sicuriellovir@users.noreply.github.com>
2020-12-03 13:37:28 -0500

    Update cmdclose.c

--- Project3/cmdclose.c
+++ Project3/cmdclose.c
@@ -3,28 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void cmdclose(unsigned int pwdStartCluster, char* dirName, int fatFile_fp, struct BPBInfo* info, struct openFile *head, struct openFile *ptr)
+void cmdclose(char* fileName, struct openFile *head)
 {
-    struct openFile *secondPtr;
-    int i;
-    for(i = strlen(dirName); i < 11; i++)
-        strcat(dirName, " ");
+    struct openFile* temp = NULL;
 
-
-    if(OpenFile(dirName, head) == 0)
-        printf("File not found.\n");
-    else
+    for (struct openFile* i = head; i != NULL && temp == NULL; i = i->next) 
     {
-        for(ptr = head; ptr !=NULL; secondPtr = ptr)
-        {
-            if (strncmp(ptr->entry->DIR_name, dirName, 11) != 0)
-                continue;
-            if(secondPtr != NULL)
-                secondPtr->next = ptr->next;
-            free(ptr);
-            printf("File Closed\n");
-            return;
-        }
-
+        if (!strcasecmp(fileName, i->entry->DIR_name))
+                Remove(fileName, head);
+        else
+            printf("Error: could not close file\n");
     }
 }
